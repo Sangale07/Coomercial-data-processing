a6ae5ceb676db181feb0e7a3ac0f50fe8f8b694b
rushi <rushikeshsangale57@gmail.com>
2024-08-25 19:46:41 +0530

    CompanyShares class Created

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Stock Account Program");
 
     }
 
+}
+ class CompanyShares {
+    private String stockSymbol;
+    private int numberOfShares;
+    private LocalDateTime transactionDateTime;
+
+    public CompanyShares(String stockSymbol, int numberOfShares, LocalDateTime transactionDateTime) {
+        this.stockSymbol = stockSymbol;
+        this.numberOfShares = numberOfShares;
+        this.transactionDateTime = transactionDateTime;
+    }
 }
